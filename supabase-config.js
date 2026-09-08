f2ba075a8366c06dd5dfefe0c41156315cedec0e
hisnationalanthem <potatolizard123@gmail.com>
2026-09-08 20:40:09 -0300

    Create supabase-config.js

---
+++ supabase-config.js
@@ -0,0 +1,5 @@
+export const SUPABASE_URL =
+  "https://ydtbmzffqedgbcsbreqj.supabase.co/functions/v1/submit-commission";
+
+export const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_VjcfVGGDdYhePZNeCGypGg_Cym-nguy";
